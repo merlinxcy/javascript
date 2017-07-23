f2ad76ad2b4e6e9ca8e823531b7479a55feca72b
Xeldax <1445126114@qq.com>
2017-07-23 13:43:45 +0800

    Update studytest.js

--- studytest.js
+++ studytest.js
@@ -22,10 +22,51 @@ function change2()
 }
 function validateform()
 {
-  var x=document.forms['myform']['fname'].value;
+  var x=document.forms['myForm']['fname'].value;
   alert(x);
 }
+function validateform2()
+{
+  var x=document.getElementById('fname');
+  alert(x.innerHTML);
+}
 
+
+function veryfyemail()
+{
+    var x=document.forms["myForm"]["email"].value;
+    var atpos=x.indexOf("@");
+    var dotpos=x.lastIndexOf(".");
+    if(atpos<1||dotpos<atpos+2||dotpos+2>x.length)
+    {
+      alert("这不是一个有效的email地址");
+      return false;
+    }
+
+}
+/*
+function apifunction()
+{
+ document.getElementById("demo").innerHTML="1232";
+  var in=document.getElementById("id1");
+  if(in.checkValidity()==false)
+  {
+    document.getElementById("demo").innerHTML=in.validationMessage;
+  }
+}
+*/
+
+//#######################
+//JSON
+function jsontest()
+{
+    var text='{"a":11111,"b":222222,"c":333333}';
+    obj=JSON.parse(text);
+    document.getElementById("demo").innerHTML=obj.a;
+}
+//########################
+///////////////////////////////////////////////////////////
+//alert(document.getElementById("demo").innerHTML);//由于html页面还没有渲染所以demo不存在
 document.write("<b>123</b>");//html
 for(i=0;i<10;i++)
 {
@@ -37,17 +78,29 @@ for(i in a)
   document.write(i);
 }
 document.write(typeof new Date());
-console.console.log('youdingme');
+console.log('youdingme');
+//document.write("<hr>");
+//jsontest();
+////////////////////////////////////////////////////////////
 </script>
 </head>
 <body>
   <p id='aaa' onmouseover="change1()" onmouseout="change2()">aaaa</p>
   <span name='bbb'>bbb</span>
   <button type='button' onclick='myFunction()' >click here!</button>
+  <br>
+  <br>
+  <hr>
+  <form name="myForm" action="" onsubmit="apifunction()" method="">
+    <input type="text" value="xxx" name="fname" id="fname1">
+    <br>
+    <input type="text" value="xxx" name="email" id="email1">
+    <br>
+    <input type="text" id="id1" min="100" max="300" required>
+    <br>
+    <input type="submit" value="submit">
+  </form>
+  <p id="demo" name="demo" >提示信息</p>
+ <script>jsontest();</script>
 </body>
-<form name="myForm" action="" onsubmit="return validateform()" method="">
-  <input type="text" value="xxx" name="fname" id="fname1">
-  <input type="submit" value="submit">
-</form>
-  
 </html>
